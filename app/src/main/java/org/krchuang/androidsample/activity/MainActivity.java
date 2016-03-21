e315b19b3a2789abcc4b3865415f71c272c4821d
Allen Chuang <krchuang@gmail.com>
2016-03-22 00:19:01 +0800

    Added ItemFragment

--- app/src/main/java/org/krchuang/androidsample/activity/MainActivity.java
+++ app/src/main/java/org/krchuang/androidsample/activity/MainActivity.java
@@ -1,10 +1,9 @@
-package org.krchuang.androidsample;
+package org.krchuang.androidsample.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import org.krchuang.androidsample.R;
+import org.krchuang.androidsample.dummy.DummyContent;
+import org.krchuang.androidsample.fragment.ItemFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        ItemFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ItemFragment itemFragment = ItemFragment.newInstance(2);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.view_container, itemFragment, null)
+                .commit();
     }
 
     @Override
@@ -97,5 +108,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
